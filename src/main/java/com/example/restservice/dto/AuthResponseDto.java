4efda41570f5232852eaa589da1e67862a7d755e
Neil McWilliam <nmcwilliam@praxes.ca>
2023-08-22 10:38:03 -0300

    Fixing Maven

---
+++ src/main/java/com/example/restservice/dto/AuthResponseDto.java
@@ -0,0 +1,15 @@
+package com.example.restservice.dto;
+
+import lombok.Data;
+
+@Data
+public class AuthResponseDto {
+
+    private String accessToken;
+    private String tokenType = "Bearer ";
+
+    public AuthResponseDto(String accessToken) {
+        this.accessToken = accessToken;
+    }
+
+}
